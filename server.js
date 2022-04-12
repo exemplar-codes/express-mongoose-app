48fbb94b1ccbe493c809adfe0515211c9fcfd0ec
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-04-13 03:04:52 +0530

    Add root file and route

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const port = 3002;
+
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  res.sendFile("./index.html");
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
